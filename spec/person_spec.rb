435a5c3b6bb2d0660a8268a4f0b5010a46eb68cf
AbdifatahYasin1 <iskatag123@gmail.com>
2023-04-08 23:10:20 +0300

    updated

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -5,18 +5,11 @@ describe Person do
     person = Person.new(age: 18, name: 'Ali')
     expect(person).to be_an_instance_of Person
   end
-
-
-
-
   describe 'should return the correct name' do
     it 'should return the correct name' do
       person = Person.new(name: 'Ali', age: 20)
       expect(person.correct_name).to eq 'Ali'
     end
-
-
-
     describe 'should return true if the person is of age or has permission' do
       it 'should return true if the person is of age' do
         person = Person.new(name: 'Ali', age: 20)
