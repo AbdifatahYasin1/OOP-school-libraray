f608566bb1d718861d6da3707676e36a3aef9a37
AbdifatahYasin1 <iskatag123@gmail.com>
2023-04-08 21:05:38 +0300

    fixed linter erros

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -2,10 +2,9 @@ require './classroom'
 require './student'
 
 describe Classroom do
-# write the test here
-  it "should have a label" do
-    classroom = Classroom.new("Math")
-    classroom.label.should == "Math"
+  # write the test here
+  it 'should have a label' do
+    classroom = Classroom.new('Math')
+    classroom.label.should == 'Math'
   end
-
 end
